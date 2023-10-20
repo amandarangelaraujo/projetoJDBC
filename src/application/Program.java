2604fb3faf83480e2c3a06fe98c441c0e4a20bdb
Amanda Rangel <amandarangelaraujo@gmail.com>
2023-10-20 09:31:09 -0300

    adicionado classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+    public static void main(String[] agrs){
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+
+
+}
